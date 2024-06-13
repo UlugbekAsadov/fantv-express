a4c817c2a8df14764175aced9f4a85996c3a9eda
Ulugbek <ulugb3k270@gmail.com>
2024-06-13 10:56:27 +0500

    register validation improved

--- src/schemas/auth/register.schema.ts
+++ src/schemas/auth/register.schema.ts
@@ -2,19 +2,30 @@ import Joi, { ObjectSchema } from 'joi';
 import { AuthType } from '../../utils/enums/auth.enum';
 
 const registerSchema: ObjectSchema = Joi.object().keys({
-  username: Joi.string().required().min(2).messages({
-    'string.base': 'username must be of type string',
-    'string.min': 'username must be at least 2 characters long',
-    'string.empty': 'username is a required field',
-  }),
+  username: Joi.string()
+    .required()
+    .min(2)
+    .custom((value) => {
+      const uppercaseRegex = /[A-Z]/;
+      if (uppercaseRegex.test(value)) {
+        throw Error('Username must not contain uppercase characters');
+      }
+      return value;
+    })
+    .alphanum()
+    .messages({
+      'string.base': 'username must be of type string',
+      'string.min': 'username must be at least 2 characters long',
+      'string.empty': 'username is a required field',
+    }),
   fullName: Joi.string().required().min(2).messages({
     'string.base': 'fullName must be of type string',
     'string.min': 'fullName must be at least 2 characters long',
     'string.empty': 'fullName is a required field',
   }),
-  password: Joi.string().required().min(6).messages({
+  password: Joi.string().required().min(6).alphanum().messages({
     'string.base': 'password must be of type string',
-    'string.min': 'password must be at least 2 characters long',
+    'string.min': 'password must be at least 6 characters long',
     'string.empty': 'password is a required field',
   }),
   phoneNumber: Joi.string().required().messages({
