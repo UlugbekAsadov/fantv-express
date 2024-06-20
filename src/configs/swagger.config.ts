af4f99f16c0a2cb143b7d85fd949b99142ad4fe2
Ulugbek <ulugb3k270@gmail.com>
2024-06-20 08:56:31 +0500

    removed unnecessary codes

--- src/configs/swagger.config.ts
+++ src/configs/swagger.config.ts
@@ -1,5 +1,4 @@
 import swaggerJsDoc from 'swagger-jsdoc';
-import { config } from './index';
 
 const swaggerDefinition = {
   openapi: '3.1.0',
@@ -7,11 +6,6 @@ const swaggerDefinition = {
     title: 'Fan Tv API',
     version: '0.0.1',
   },
-  servers: [
-    {
-      url: config.BASE_ORIGIN + ':' + config.PORT + config.BASE_PATH,
-    },
-  ],
 };
 
 const options = {
